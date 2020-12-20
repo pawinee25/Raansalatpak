ba831e2ccb0bdf58d73b5ce904c7d3fc92cc883c
patthum <patthum.2540@gmail.com>
2020-12-20 11:11:07 +0700

    test2

--- app/src/main/java/com/example/raansalatpak/ConnectDB.java
+++ app/src/main/java/com/example/raansalatpak/ConnectDB.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 
 public class ConnectDB {
     public static Connection getConnection() {
-        return Dru.connection("192.168.43.127","pawinee","123456","healthy");
+        return Dru.connection("172.20.10.6","pawinee","123456","healthy");
     }
 
 }
