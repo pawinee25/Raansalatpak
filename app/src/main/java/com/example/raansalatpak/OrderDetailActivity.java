591665d70b093751ac27cf0542a1248eb3303711
naridsara <bisrychaikong@gmail.com>
2021-01-17 11:44:35 +0700

    10/1/2564 คลิกเพิ่มเเละลดจำนวนอาหารได้เเล้วโชว์รายละเอียดอาหารได้เเล้วเปลี่ยนรูปโลโก้เเล้ว

--- app/src/main/java/com/example/raansalatpak/OrderDetailActivity.java
+++ app/src/main/java/com/example/raansalatpak/OrderDetailActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.adedom.library.Dru;
+import com.adedom.library.ExecuteQuery;
 import com.example.raansalatpak.Model.Product;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
     private TextView mTvNumberCounter;
+    private static final String TAG = "OrderDetailActivity";
     private int Count = 0 ;
     private ImageView mImg_Imgae;
     private  ImageView iv_imagefood;
@@ -26,6 +32,7 @@ public class OrderDetailActivity extends AppCompatActivity {
     private Product product;
     private Button mBtnDecrement;
     private Button mBtnIncrement;
+    private TextView mTv_Detail_Food_Name;
 
 
     @Override
@@ -33,18 +40,10 @@ public class OrderDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
 
+
         mTvNumberCounter = (TextView) findViewById(R.id.tv_number_counter);
         mBtnDecrement = (Button)findViewById(R.id.btn_decrement);
         mBtnIncrement = (Button)findViewById(R.id.btn_increment);
-
-
-        String Food_ID = getIntent().getStringExtra("Food_ID");
-        String Food_Name = getIntent().getStringExtra("Food_Name");
-        String Food_NameUS = getIntent().getStringExtra("Food_NameUS");
-        String Food_Detail_ID = getIntent().getStringExtra("Food_Detail_ID");
-        String Food_Image = getIntent().getStringExtra("Food_Image");
-        int Food_Price = getIntent().getIntExtra("Food_Price",0);
-
         mImg_Imgae = (ImageView) findViewById(R.id.img_imgae);
         mTv_NameFoodTh = (TextView) findViewById(R.id.tv_namefoodth);
         mTv_NameFoodUs = (TextView) findViewById(R.id.tv_namefoodus);
@@ -52,14 +51,40 @@ public class OrderDetailActivity extends AppCompatActivity {
         mTv_Food_Price = (TextView)findViewById(R.id.tv_food_price);
 
 
+
+
+        String Food_ID = getIntent().getStringExtra("Food_ID");
+        String Food_Name = getIntent().getStringExtra("Food_Name");
+        String Food_NameUS = getIntent().getStringExtra("Food_NameUS");
+        int Food_Detail_ID = getIntent().getIntExtra("Food_Detail_ID",0);
+        String Food_Image = getIntent().getStringExtra("Food_Image");
+        int Food_Price = getIntent().getIntExtra("Food_Price",0);
         mTv_NameFoodTh.setText(Food_Name);
         mTv_NameFoodUs.setText(Food_NameUS);
-        mTv_Detail_Food.setText(Food_Detail_ID);
+        //mTv_Detail_Food.setText(Food_Detail_ID);
         mTv_Food_Price.setText(Food_Price+"");
-        Dru.loadImageCircle(mImg_Imgae, Food_Image );
-        //Dru.loadImageCircle(holder.mImg_Imgae,  product.getImagefood());
-        //Dru.loadImageCircle(mImg_Imgae, ConnectDB.BASE_URL  +product.getImagefood());
-       // mImg_Imgae.setImageDrawable(Drawable.createFromPath(Dru.loadImageCircle(holder.mImg_Imgae,  product.getImagefood())));
+        Dru.loadImageCircle(mImg_Imgae,Food_Image);
+        String sql ="SELECT Food_Detail_Properties FROM food  INNER JOIN fooddetail ON fooddetail.Food_Detail_ID & food.Food_Detail_ID = '"+Food_Detail_ID+"'";
+        //String sql = "SELECT Food_Detail_Properties FROM food  INNER JOIN fooddetail ON food.Food_Detail_ID = '"+Food_Detail_ID+"' and fooddetail.Food_Detail_ID = '"+Food_Detail_ID+"'";
+        Dru.connection(ConnectDB.getConnection())
+                .execute(sql)
+                .commit(new ExecuteQuery() {
+                    @Override
+                    public void onComplete(ResultSet resultSet) {
+                        try {
+                            while (resultSet.next()) {
+                                Log.d(TAG, "onComplete: "+Food_Detail_ID );
+                                mTv_Detail_Food.setText(resultSet.getString("Food_Detail_Properties"));
+
+
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
 
         mBtnIncrement.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,15 +111,4 @@ public class OrderDetailActivity extends AppCompatActivity {
         mTvNumberCounter.setText(""+Count);
     }
 
-
-//    public  void increment(View v){
-//        Count++;
-//        mTvNumberCounter.setText(""+Count);
-//
-//    }
-//    public void decrement(View v){
-//        if(Count <= 0) Count = 0;
-//        else Count--;
-//        mTvNumberCounter.setText(""+Count);
-//    }
 }
