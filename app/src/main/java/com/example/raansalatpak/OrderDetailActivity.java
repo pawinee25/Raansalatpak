581483703b5855845c26ccad6cf9d3159222b4b5
naridsara <bisrychaikong@gmail.com>
2021-01-24 19:54:10 +0700

    10/1/2564 ใส่ตระกร้าได้เเล้ว

--- app/src/main/java/com/example/raansalatpak/OrderDetailActivity.java
+++ app/src/main/java/com/example/raansalatpak/OrderDetailActivity.java
@@ -123,6 +123,8 @@ public class OrderDetailActivity extends AppCompatActivity {
         if (Count <= 0) Count = 0;
         else Count--;
         mTvNumberCounter.setText("" + Count);
+        mTotal_Price = String.format("%d", Food_Price * Count);
+        mTv_Food_Price.setText(mTotal_Price);
     }
 
     private void increment() {
